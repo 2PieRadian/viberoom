b34f500921571058498ba5da6f885fb75899c042
2PieRadian <kataware.doki2005@gmail.com>
2026-01-10 01:35:51 +0530

    join create room components completed almost

--- app/components/CreateRoomCard.tsx
+++ app/components/CreateRoomCard.tsx
@@ -1,7 +1,24 @@
 export default function CreateRoomCard() {
   return (
-    <div className="w-full h-[200px] bg-room-card rounded-[10px] flex items-center justify-center">
-      Create Room Card
+    <div className="w-full bg-room-card rounded-[10px] p-[25px]">
+      <div className="flex flex-col gap-[5px] w-full">
+        <label
+          htmlFor="roomName"
+          className="text-base text-room-card-input-label"
+        >
+          Room Name
+        </label>
+        <input
+          type="text"
+          id="roomName"
+          className="bg-room-card-input max-w-[600px] text-md w-full px-[16px] border-[1px] border-room-card-input-border rounded-[8px] px-[10px] py-[8px]"
+          placeholder="Enter room name"
+        />
+      </div>
+
+      <div className="bg-cerulean-blue-700 max-w-[600px] text-md w-full rounded-[8px] mt-[15px] py-[10px] text-center cursor-pointer text-md">
+        Create Room
+      </div>
     </div>
   );
 }
--- app/components/JoinRoomCard.tsx
+++ app/components/JoinRoomCard.tsx
@@ -1,7 +1,7 @@
 export default function JoinRoomCard() {
   return (
-    <div className="w-full bg-room-card rounded-[10px] flex p-[25px]">
-      <div className="flex flex-col gap-[5px]">
+    <div className="w-full bg-room-card rounded-[10px] p-[25px]">
+      <div className="flex flex-col gap-[5px] w-full">
         <label
           htmlFor="roomId"
           className="text-base text-room-card-input-label"
@@ -11,9 +11,13 @@ export default function JoinRoomCard() {
         <input
           type="text"
           id="roomId"
-          className="bg-room-card-input px-[16px] border-[1px] border-room-card-input-border rounded-[8px] px-[10px] py-[8px]"
+          className="bg-room-card-input max-w-[600px] text-md w-full px-[16px] border-[1px] border-room-card-input-border rounded-[8px] px-[10px] py-[8px]"
           placeholder="Enter room id here"
         />
+      </div>
+
+      <div className="bg-cerulean-blue-700 max-w-[600px] text-md w-full rounded-[8px] mt-[15px] py-[10px] text-center cursor-pointer text-md">
+        Join Room
       </div>
     </div>
   );
