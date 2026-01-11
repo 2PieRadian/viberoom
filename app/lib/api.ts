0fde7a3fe968fee74ecaa676450fa6cc89bcb246
2PieRadian <kataware.doki2005@gmail.com>
2026-01-11 22:14:28 +0530

    backend url

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -1,1 +1,2 @@
-export const BACKEND_URL = "https://viberoom-backend-production.up.railway.app";
+export const BACKEND_URL = "viberoom-backend-production-793f.up.railway.app";
+// export const BACKEND_URL = "http://localhost:8080";
